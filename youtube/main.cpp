bca90dbc47e13783c41395325b237a4637aad709
Parag K Mital <parag@pkmital.com>
2011-08-24 16:47:20 +0100

    adding video and extra

---
+++ youtube/main.cpp
@@ -0,0 +1,37 @@
+#include <string.h>
+#include <iostream.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+	// get youtube top 20
+	system("wget http://www.youtube.com/charts");
+	// parse for links
+	system("cat charts | grep data-video-ids= > out.txt");
+
+	FILE *fp  = fopen("out.txt", "r");
+	char buf[1024];
+	int i = 1;
+	while(fscanf(fp, "<a href=%s \n", buf) != EOF)
+	{
+		char buf2[1024];
+		printf("read: %s", buf);
+		
+		// download video
+		sprintf(buf2, "python youtube.py www.youtube.com%s -f 18 -o %d.mp4", buf, i);
+		system(buf2);
+		fgets(buf2, 10000, fp);
+		
+		// strip audio
+		sprintf(buf2, "ffmpeg -i %d.mp4 -vn -acodec pcm_s16le -ar 44100 -ac 2 %d.wav", i, i);
+		system(buf2);
+		
+		// strip video
+		
+		i++;
+	}
+	fclose(fp);
+}
+
